c572bf73ae84731975c43df271ea0e631b419f34
Tohid <tohidulislam202013@gmail.com>
2026-09-21 10:10:05 +0600

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/A-E-commerce-website-for-learning-with-tailwind-css/',
   plugins: [
-    tailwindcss(),
+    tailwindcss()
   ],
 })
