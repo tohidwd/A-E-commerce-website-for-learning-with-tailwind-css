5e346c711ced5585ee6b8b1995e4add229713b28
Tohid <tohidulislam202013@gmail.com>
2026-09-22 09:22:35 +0600

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss()
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        img1: resolve(__dirname, 'img1.html'),
+        img2: resolve(__dirname, 'img2.html'),
+        img3: resolve(__dirname, 'img3.html'),
+        img4: resolve(__dirname, 'img4.html'),
+        img5: resolve(__dirname, 'img5.html'),
+        img6: resolve(__dirname, 'img6.html'),
+      },
+    },
+  },
 })
